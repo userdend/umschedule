01429c04f27f884af19b0961c8ce7dccb11acc84
userdend <boydbayndend99@gmail.com>
2021-03-15 06:59:30 +0800

    adding files

---
+++ App.js
@@ -0,0 +1,263 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+  FlatList,
+  TextInput,
+  TouchableOpacity,
+  Dimensions,
+  ImageBackground,
+} from 'react-native';
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import database from '@react-native-firebase/database';
+import {WebView} from 'react-native-webview';
+import NetInfo from '@react-native-community/netinfo';
+import Swiper from 'react-native-swiper';
+import SplashScreen from 'react-native-splash-screen';
+import AwesomeLoading from 'react-native-awesome-loading';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      storeList: [{currentSchedule: '', path: '', subject: ''}],
+      list: [],
+      subjectLink: '',
+      currentItem: '',
+      screenWidth: Dimensions.get('screen').width,
+      screenHeight: Dimensions.get('screen').height,
+      statusType: 'Fetching data.',
+      statusBackgroundColor: '#85929E',
+      displayFlatList: 'none',
+      webViewImageZindex: 2,
+      loadingIsActive: true,
+      showsButtons: false,
+    };
+  }
+  // Load data to state.
+  componentDidMount() {
+    SplashScreen.hide();
+    let temp = {tempList: []};
+    this.setState({storeList: []});
+    // Get network info.
+    NetInfo.addEventListener((state) => {
+      if (state.type === 'none' || state.type === 'unknown') {
+        this.setState({
+          statusType: 'No Network Connection.',
+          statusBackgroundColor: '#CD6155',
+          loadingIsActive: false,
+        });
+      }
+    });
+    // Get data from firebase.
+    database()
+      .ref('semester')
+      .on('value', (snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+          temp.tempList = [...temp.tempList, childSnapshot.val()];
+        });
+        // Set list value as storeList, filter storeList to be display in list.
+        this.setState({storeList: temp.tempList});
+        this.setState({
+          list: this.state.storeList,
+          statusType:
+            'Current Schedule: ' + this.state.storeList[0].currentSchedule,
+          statusBackgroundColor: '#5DADE2',
+          loadingIsActive: false,
+          subjectLink: this.state.storeList[0].subjectLink,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <View style={[style.body, {height: this.screenHeight}]}>
+        <StatusBar backgroundColor={'#303F9F'} barStyle={'light-content'} />
+        <View style={style.header}>
+          <Text style={style.title}>UMS SCHEDULE</Text>
+          <TextInput
+            style={style.textInput}
+            placeholder="search."
+            onChangeText={(value) => {
+              let getResult = this.state.storeList.filter((item) => {
+                return (
+                  item.subject.toUpperCase().indexOf(value.toUpperCase()) > -1
+                );
+              });
+              this.setState({list: getResult, displayFlatList: 'flex'});
+              if (value.length === 0) {
+                this.setState({
+                  displayFlatList: 'none',
+                  webViewImageZindex: 2,
+                });
+              }
+            }}
+          />
+          <Text
+            style={[
+              style.status,
+              {backgroundColor: this.state.statusBackgroundColor},
+            ]}>
+            {this.state.statusType}
+          </Text>
+        </View>
+        <Swiper
+          index={0}
+          loop={false}
+          scrollEnabled={false}
+          showsButtons={this.state.showsButtons}
+          ref={(swiper) => (this.swiper = swiper)}>
+          <View
+            style={[
+              style.majorComponent,
+              {
+                width: this.state.screenWidth,
+              },
+            ]}>
+            <View
+              style={[
+                style.imageContainer,
+                {
+                  width: this.state.screenWidth,
+                },
+              ]}>
+              <AwesomeLoading
+                indicatorId={20}
+                size={100}
+                isActive={this.state.loadingIsActive}
+                text="Connecting, please wait."
+              />
+              <ImageBackground
+                style={[style.imageBg, {top: '0%'}]}
+                source={require('./assets/bg_image/bg_flatlist.png')}>
+                <View
+                  style={{
+                    width: this.state.screenWidth / 1.2,
+                    height: this.state.screenWidth / 1.2,
+                  }}></View>
+              </ImageBackground>
+            </View>
+            <View style={style.stackedComponent}>
+              <FlatList
+                style={{display: this.state.displayFlatList}}
+                data={this.state.list}
+                initialNumToRender={11}
+                renderItem={({item}) => (
+                  <TouchableOpacity
+                    style={style.touchableComponent}
+                    onPress={() => {
+                      this.swiper.scrollBy(1, true);
+                      this.setState({
+                        currentItem: item.path,
+                        webViewImageZindex: 0,
+                        showsButtons: true,
+                      });
+                    }}>
+                    <Text style={style.subjectName}>{item.subject}</Text>
+                  </TouchableOpacity>
+                )}
+                keyExtractor={(item) => item.subject}
+              />
+            </View>
+          </View>
+          <View style={[style.majorComponent, {width: this.state.screenWidth}]}>
+            <View
+              style={[
+                style.imageContainer,
+                {
+                  width: this.state.screenWidth,
+                  height: this.state.screenHeight,
+                  backgroundColor: '#fff',
+                  zIndex: this.state.webViewImageZindex,
+                },
+              ]}>
+              <ImageBackground
+                style={style.imageBg}
+                source={require('./assets/bg_image/bg_webview.png')}>
+                <View
+                  style={{
+                    width: this.state.screenWidth / 1.2,
+                    height: this.state.screenWidth / 1.2,
+                  }}></View>
+              </ImageBackground>
+            </View>
+            <View
+              style={[
+                style.stackedComponent,
+                {
+                  width: this.state.screenWidth,
+                  zIndex: 1,
+                },
+              ]}>
+              <WebView
+                source={{uri: this.state.subjectLink + this.state.currentItem}}
+              />
+            </View>
+          </View>
+        </Swiper>
+      </View>
+    );
+  }
+}
+
+const style = StyleSheet.create({
+  body: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: '#3F51B5',
+  },
+  title: {
+    marginLeft: 20,
+    marginTop: 10,
+    color: '#FFF',
+    fontWeight: 'bold',
+  },
+  textInput: {
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    margin: 20,
+    marginTop: 10,
+  },
+  status: {
+    textAlign: 'center',
+    padding: 5,
+    color: '#FFF',
+  },
+  touchableComponent: {
+    borderColor: '#D5D8DC',
+    borderBottomWidth: 1,
+    backgroundColor: '#FFF',
+  },
+  subjectName: {
+    margin: 10,
+  },
+  imageContainer: {
+    position: 'absolute',
+    height: '75%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageBg: {
+    display: 'flex',
+    resizeMode: 'cover',
+    top: '-20%',
+  },
+  stackedComponent: {
+    position: 'absolute',
+    height: '100%',
+  },
+  majorComponent: {
+    height: '100%',
+    backgroundColor: '#FFF',
+  },
+});
